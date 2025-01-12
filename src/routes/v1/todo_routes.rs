273fe980593e300f50a71da24d0ec411f25c02e4
yusufocaliskan <yusufocaliskan@gmail.com>
2025-01-12 17:11:35 +0300

    finalyzing

--- src/routes/v1/todo_routes.rs
+++ src/routes/v1/todo_routes.rs
@@ -1,10 +1,17 @@
 use crate::controllers::todo_controller::TodoController as controller;
+use crate::middlewares::auth_middleware::auth_validator;
 use actix_web::web;
+use actix_web_httpauth::middleware::HttpAuthentication;
 
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/todos")
-            .route("", web::post().to(controller::create_todo))
+            .route(
+                "",
+                web::post()
+                    .to(controller::create_todo)
+                    .wrap(HttpAuthentication::bearer(auth_validator)),
+            )
             .route("", web::get().to(controller::get_all_todos))
             .route("/{id}", web::get().to(controller::get_todo))
             .route("/{id}", web::put().to(controller::update_todo))
